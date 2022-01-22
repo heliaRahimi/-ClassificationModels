63ebe25e2ebc8d99145cfc7ffbe89256bb49b733
henrywan1920 <65926241+henrywan1920@users.noreply.github.com>
2022-01-22 18:01:08 -0500

    CodeBase v0.1

---
+++ Common.py
@@ -0,0 +1,14 @@
+"""
+Common functions which will be used by any models.
+"""
+DATESET_PATH = "./resources"
+
+
+def read_csv(file_name):
+    """
+    Read a csv file.
+    Parameters
+    """
+    dataset_path = "?/?".format(DATESET_PATH, file_name)
+    csv = None
+    return csv
---
+++ DecisionTree.py
@@ -0,0 +1,10 @@
+from sklearn.model_selection import GridSearchCV
+from Common import read_csv
+
+
+def main():
+    print("Hello")
+
+
+if __name__ == '__main__':
+    main()
